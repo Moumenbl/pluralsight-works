e55f558c330db7386c41553d8a7739654471329b
Moumenbl <Moumen.boulahbal@gmail.com>
2020-07-05 23:28:54 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -10,7 +10,18 @@ xhr.onload = function () {
 	if (xhr.status === 200) {
 		var myStuff = JSON.parse(xhr.responseText);
 		console.log(myStuff);
+
+			for (i = 0; i < myStuff.presidents.length; i++) {
+			console.log(myStuff.presidents[i].first);
+			console.log(myStuff.presidents[i].last);
+			console.log(myStuff.presidents[i].served);
+
+			console.log(myStuff.vicepresidents[i].first);
+			console.log(myStuff.presidents[i].last);
+		}// end loop
+
 	}// end if
+
 }// end func
 
 
