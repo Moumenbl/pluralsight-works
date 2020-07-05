f87e22c15f03622d45e409d49cb979481fece484
Moumenbl <Moumen.boulahbal@gmail.com>
2020-07-05 17:23:28 +0200

    message

--- js/main.js
+++ js/main.js
@@ -3,18 +3,11 @@
 var  dataReq = new XMLHttpRequest();
 
 dataReq.open('GET', 'data.JSON', true);
-dataReq.responseText = 'text';
+dataReq.responseType = 'text';
 
-
-dataReq.onreadystatechange = function () {
-	console.log(dataReq.readyState);
-	console.log(dataReq.status);
-	console.log(dataReq.statusText);
-}//end function
 
 dataReq.onload = function () {
-
-	if (dataReq === 200) {
+	if (dataReq.status === 200) {
 		var myStuff = JSON.parse(dataReq.responseText);
 		console.log(myStuff);
 	}//end if
