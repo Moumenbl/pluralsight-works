6cbd458fbf048f570b52be593184b2a8011268e4
Moumenbl <Moumen.boulahbal@gmail.com>
2020-07-05 23:23:45 +0200

    new

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ xhr.onload = function () {
 	if (xhr.status === 200) {
 		var myStuff = JSON.parse(xhr.responseText);
 		console.log(myStuff);
-		console.log(myStuff[i]);
 	}// end if
 }// end func
 
