ae01a00987fc623ef3866f726e6897ec716e4a94
Moumenbl <Moumen.boulahbal@gmail.com>
2020-07-08 15:25:55 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,40 +1,19 @@
 // JavaScript Document 
+var hotelInfo;
+var details;
 
 var xhr = new XMLHttpRequest();
-
 xhr.open('GET', 'data.JSON', true);
 xhr.responseType = 'text';
 xhr.send();
 
-xhr.onload = function () {
-	if (xhr.status === 200) {
-		var myStuff = JSON.parse(xhr.responseText);
-		console.log(myStuff);
-
-			for (i = 0; i < myStuff.presidents.length; i++) {
-			console.log(myStuff.presidents[i].first);
-			console.log(myStuff.presidents[i].last);
-			console.log(myStuff.presidents[i].served);
-
-			console.log(myStuff.vicepresidents[i].first);
-			console.log(myStuff.vicepresidents[i].last);
-		}// end loop
-
-		var myString = "";
-		for (i = 0; i < myStuff.presidents.length; i++) {
-			var x = i + 1;
-			myString += "President"+ x +" was ";
-			myString += myStuff.presidents[i].first + " ";
-			myString += myStuff.presidents[i].last +" ";
-
-			myString += "He served from " + myStuff.presidents[i].served + " with ";
-			myString += myStuff.vicepresidents[i].first + " ";
-			myString += myStuff.vicepresidents[i].last + " ";
-		}
-		document.getElementById('demo'). innerHTML = myString;
+xhr.onload = function (){
+	if(xhr.status === 200){
+		hotelInfo = JSON.parse(xhr.responseText);
+		console.log(hotelInfo);
 	}// end if
+}// end Func
 
-}// end func
 
 
 
