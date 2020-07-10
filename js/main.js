3805ceb6e0849c8bcda7a815f0df51743bff9482
Moumenbl <Moumen.boulahbal@gmail.com>
2020-07-10 16:13:41 +0200

    update 1.2b

--- js/main.js
+++ js/main.js
@@ -24,16 +24,12 @@ function display(x){
 	document.getElementById('weekday').innerHTML = hotelInfo[x].cost.weekday;
 	document.getElementById('weekend').innerHTML = hotelInfo[x].cost.weekend;
 
-	var details = "";
+	details = "";
 	for ( i = 0; i < hotelInfo[x].details.length; i++) {
-		details += "<p>" + hotelInfo.details[i] + "</p>";
+		details += "<p>" + hotelInfo[x].details[i] + "</p>";
 	}//end loop
 
 	document.getElementById('details').innerHTML = details;
-
-
-
-
 
 }
 
